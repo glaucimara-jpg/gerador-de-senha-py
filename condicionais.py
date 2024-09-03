4c981076f46a5c807245ee39e6fa68e9155a553b
Professora Glau <80214705+glaucimara-jpg@users.noreply.github.com>
2024-09-03 15:35:02 -0300

    Update condicionais.py

--- condicionais.py
+++ condicionais.py
@@ -9,3 +9,25 @@ def gerar_senha(comprimento):
 # Exemplo de uso: gerar senha com comprimento 12
 senha_gerada = gerar_senha(12)
 print("Senha gerada:", senha_gerada)
+
+# segundo código 
+
+def sequ(n):
+    aux1 = 2
+    aux2 = 3.0
+    soma = 0
+
+    while aux1 <= num:
+        print aux1, aux2
+        soma = soma + aux1/aux2
+        aux1 = aux1 + 1
+        aux2 = aux2 + 2
+
+    return soma
+
+num = input('Digite um valor: ')
+while num < 0:
+    num = input('Digite um valor positivo: ')
+
+res = sequ(num)
+print res
