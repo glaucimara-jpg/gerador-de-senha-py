c6ee01e3ac6fedca6eecf7a105e4cf7412cc06b7
Professora Glau <80214705+glaucimara-jpg@users.noreply.github.com>
2024-09-03 15:24:26 -0300

    Update condicionais.py

--- condicionais.py
+++ condicionais.py
@@ -1,27 +1,11 @@
-from tkinter import *
+import random
+import string
 
-class Application:
-    def __init__(self, master=None):
-        self.widget1 = Frame(master)
-        self.widget1.pack()
-        self.msg = Label(self.widget1, text="Primeiro widget")
-        self.msg["font"] = ("Calibri", "9", "italic")
-        self.msg.pack ()
-        self.sair = Button(self.widget1)
-        self.sair["text"] = "Clique aqui"
-        self.sair["font"] = ("Calibri", "9")
-        self.sair["width"] = 10
-        self.sair["command"] = self.mudarTexto
-        self.sair.pack ()
+def gerar_senha(comprimento):
+    caracteres = string.ascii_letters + string.digits + string.punctuation
+    senha = ''.join(random.choice(caracteres) for _ in range(comprimento))
+    return senha
 
-    def mudarTexto(self):
-        if self.msg["text"] == "Primeiro widget":
-            self.msg["text"] = "O botão recebeu um clique"
-        else:
-            self.msg["text"] = "Primeiro widget"
-
-
-
-root = Tk()
-Application(root)
-root.mainloop()
+# Exemplo de uso: gerar senha com comprimento 12
+senha_gerada = gerar_senha(12)
+print("Senha gerada:", senha_gerada)
